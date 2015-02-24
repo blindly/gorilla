971be4a890aafef43b1a13ae84ef7cf610813ea9
blindly <wk@mailsetup.us>
2015-02-24 13:30:18 -0500

    adding template files

---
+++ application/views/templates/header.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Gorilla APP</title>
+        
+        <!-- CSS -->
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+
+        <h1>Gorilla App</h1>
---
+++ application/views/user/index.php
@@ -0,0 +1,1 @@
+<h2>Hi <?php echo gorillaUuid ?></h2>
