2c90b0cd538c892becc899e462f371df6ed4f459
blindly <wk@mailsetup.us>
2015-02-24 17:40:09 -0500

    adding javascript

--- static/js/expenses.js
+++ static/js/expenses.js
@@ -1,4 +1,5 @@
 $( "#addition" ).click(function() {
+    /*
     $.post( "addExpenses", 
     {
         description: $('#description').val(), 
@@ -6,5 +7,27 @@ $( "#addition" ).click(function() {
         category: $('#category').val(), 
         merchant: $('#merchant').val(), 
         location: $('#location').val(), 
+    });
+    */
+    
+    var description = 'description=' . $('#description').val();
+    var amount = 'amount=' . $('#amount').val();
+    var category = 'category=' . $('#category').val();
+    var merchant = 'merchant=' . $('#merchant').val();
+    var location = 'location=' . $('#location').val();
+    var myExpenses = new Array(description, location, category, merchant, location);
+    
+    $.ajax({
+        type: "POST",
+        url: "https://gorilla.borke.us/index.php/user/expenses",
+        data: myExpenses.join('&'),
+        success: function()
+        {
+          console.log("AJAX request was successfull");
+        },
+          error:function()
+        {
+          console.log("AJAX request was a failure");
+        }
     });
 });
