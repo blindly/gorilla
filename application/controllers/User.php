10b26b68d67ea6faef12aacae4e25f5f139062df
blindly <wk@mailsetup.us>
2015-02-24 12:54:53 -0500

    Removed userguide and testing uuid

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -4,8 +4,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class User extends CI_Controller {
 
 	public function index()
-	{
-		$userUuid = uniqid();
-        echo $userUuid;
+	{        
+        $gorillaUuid = $this->input->cookie('gorillaUuid');
+        if ( ! $gorillaUuid )
+        {
+            $gorillaUuid = com_create_guid();
+            $cookie = array(
+                    'name'   => 'gorillaUuid',
+                    'value'  => $gorillaUuid,
+                    'expire' => '86500',
+                    'domain' => 'gorilla.borke.us',
+                    'path'   => '/',
+                    'prefix' => 'myprefix_',
+                    'secure' => TRUE
+            );
+
+            $this->input->set_cookie($cookie);
+        }
+        
+        echo $gorillaUuid;
 	}
 }
