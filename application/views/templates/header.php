57886f591927fc7602ee8c20fc9caeaea4dce696
blindly <wk@mailsetup.us>
2015-02-25 00:35:41 -0500

    haaaands

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <title>Gorilla App</title>
+        <title>Gorilla</title>
         
         <!-- CSS -->
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
