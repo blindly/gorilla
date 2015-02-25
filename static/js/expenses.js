2d126e8527bd93506063e299b251e116ba4478c0
blindly <wk@mailsetup.us>
2015-02-25 01:26:51 -0500

    haaaands

--- static/js/expenses.js
+++ static/js/expenses.js
@@ -78,4 +78,13 @@ $( document ).ready( function() {
            }
     });
     
+    // Get Date
+    $(".date-picker").datepicker();
+
+    $(".date-picker").on("change", function () {
+        var id = $(this).attr("id");
+        var val = $("label[for='" + id + "']").text();
+        $("#msg").text(val + " changed");
+    });
+    
 });
