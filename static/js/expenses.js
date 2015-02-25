1a778d3a6928ac04d0dc2b17fe7ae676604abbaa
blindly <wk@mailsetup.us>
2015-02-25 02:08:10 -0500

    haaaands

--- static/js/expenses.js
+++ static/js/expenses.js
@@ -80,7 +80,7 @@ $( document ).ready( function() {
     
     // Get Date
     //$(".date-picker").datepicker();
-    $( ".datepicker" ).datepicker( "option", "dateFormat", "yy-mm-dd" );
+    $(".date-picker").datepicker("option", "dateFormat", "yy-mm-dd");
 
     $(".date-picker").on("change", function () {
         var id = $(this).attr("id");
