6bbb61ac218b4fa12f8df37afe736872d24dc636
blindly <wk@mailsetup.us>
2015-02-25 01:32:15 -0500

    haaaands

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,7 +3,8 @@
         <!-- Bootstrap core JavaScript
         ================================================== -->
         <!-- Placed at the end of the document so the pages load faster -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="//code.jquery.com/ui/1.11.3/jquery-ui.min.js"></script>
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 
         <!-- Custom Scripts -->
