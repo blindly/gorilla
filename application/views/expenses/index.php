dc3a44aaba83f195f1ef2b22ce518d5deaf86a50
blindly <wk@mailsetup.us>
2015-02-25 13:38:34 -0500

    haaaands

--- application/views/expenses/index.php
+++ application/views/expenses/index.php
@@ -1,6 +1,7 @@
 <div class="container">
     
-    <div class="col-xs-6 col-lg-4" id="left-side">
+    <!-- <div class="col-xs-6 col-lg-4" id="left-side"> -->
+    <div class="col-xs-6" id="left-side">
         
         <form id="expenseForm" name="expenseForm" class="form-horizontal">
             <fieldset>
@@ -93,7 +94,8 @@
             
     </div>
     
-    <div <!-- class="col-xs-12 col-sm-6 col-lg-8" --> class="col-xs-6" id="right-side">
+    <!-- <div class="col-xs-12 col-sm-6 col-lg-8" id="right-side">  --> 
+    <div class="col-xs-6" id="right-side">
         
         <div id="messagebox" class="alert alert-success alert-dismissible" role="alert" style="display: none">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
