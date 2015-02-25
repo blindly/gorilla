d905cec8f7e0fc581998c72c85900a566c033eb7
blindly <wk@mailsetup.us>
2015-02-24 22:46:36 -0500

    haaaands

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -79,7 +79,8 @@ class User extends CI_Controller {
 
         $data = array(
             'uuid'          => $this->session->gorillaUuid,
-            'amount'        => ucfirst( $this->input->post('amount') ),
+            'amount'        => number_format((float)$this->input->post('amount'), 2, '.', ''),
+            'category'      => ucfirst( $this->input->post('category') ),
             'merchant'      => ucfirst( $this->input->post('merchant') ),
             'location'      => ucfirst( $this->input->post('location') ),
         );
