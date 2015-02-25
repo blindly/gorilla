ce3fde33d9dfee7d1b85ca0dfb125999e34975de
blindly <wk@mailsetup.us>
2015-02-25 16:01:09 -0500

    haaaands

--- application/helpers/uuid_helper.php
+++ application/helpers/uuid_helper.php
@@ -1,14 +1,8 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-if ( ! function_exists('test_method'))
+/*
+if ( ! function_exists('uuid_generator'))
 {
-    /*
-    function test_method($var = '')
-    {
-        return $var;
-    }
-    */
-    
     function uuid_generator()
     {
         if (function_exists('com_create_guid') === true)
@@ -17,6 +11,16 @@ if ( ! function_exists('test_method'))
         }
 
         return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+    }
+    
+}
+*/
+
+if ( ! function_exists('uuid_generator'))
+{
+    function uuid_generator()
+    {
+        return uniqid (rand(), true);
     }
     
 }
