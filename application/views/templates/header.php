05c21483a06d8139d3444a4c1df42200a581d93f
blindly <wk@mailsetup.us>
2015-02-24 23:16:39 -0500

    haaaands

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -28,7 +28,11 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#">Gorilla App</a>
+              <!-- <a class="navbar-brand" href="#">Gorilla App</a> -->
+                <a class="navbar-brand" rel="home" href="/" title="Gorilla App">
+                        <img style="max-width:100px; margin-top: -7px;"
+                             src="/static/img/gorilla.jpg">
+                    </a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
