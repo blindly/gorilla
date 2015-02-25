fe777e9e711e32b81daa6f6daed2cc60e8f9741d
blindly <wk@mailsetup.us>
2015-02-24 20:16:45 -0500

    adding javascript

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,7 +3,8 @@
         <!-- Bootstrap core JavaScript
         ================================================== -->
         <!-- Placed at the end of the document so the pages load faster -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 
         <!-- Custom Scripts -->
