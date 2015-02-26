4d43345b3240b1d9ba72d971d48b3993c0dd8007
blindly <wk@mailsetup.us>
2015-02-26 10:42:37 -0500

    haaaands

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -26,7 +26,10 @@
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 
         <!-- Custom Scripts -->
-        <script src="/static/js/<?= strtolower( $controller ) ?>.js"></script>
+        <?php $validScripts = array('expenses', 'mileage'); ?>
+        <?php if ( in_array(strtolower( $controller ), $validScripts) ): ?>
+            <script src="/static/js/<?= strtolower( $controller ) ?>.js"></script>
+        <?php endif; ?>
 
         <!--Load Script and Stylesheet datePicker -->
         <link type="text/css" href="/static/css/date-picker.css" rel="stylesheet" />
