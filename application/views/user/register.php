188dd6d9e3ef8de06b8432d60c428d5a47198921
blindly <wk@mailsetup.us>
2015-02-26 11:21:21 -0500

    haaaands

--- application/views/user/register.php
+++ application/views/user/register.php
@@ -2,7 +2,7 @@
 <div class="container">
     <div class="jumbotron">
         <h1>You have just registered!</h1>
-        <h2>Your UUID is <?= $gorillaUuid ?></h2>
+        <h2>Your unique identifier is <?= $gorillaUuid ?></h2>
         <p class="lead">Bookmark the link below to log your expenses.</p>
         <p><a class="btn btn-lg btn-success" href="/user/u/<?= $gorillaUuid ?>" role="button">Click to Proceed</a></p>
     </div>
