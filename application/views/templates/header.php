4ce65bd2642b7ea72d6333996dfc0e79642b95d2
blindly <wk@mailsetup.us>
2015-02-26 10:58:52 -0500

    haaaands

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -48,7 +48,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand topnav" href="#">Gorilla</a>
+                <a class="navbar-brand topnav" href="/">Gorilla</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
