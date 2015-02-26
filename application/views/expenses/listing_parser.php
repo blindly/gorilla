36dd90408213207910f5276f1c25aa47de195915
blindly <wk@mailsetup.us>
2015-02-26 16:00:13 -0500

    haaaands

--- application/views/expenses/listing_parser.php
+++ application/views/expenses/listing_parser.php
@@ -1,48 +1,50 @@
-<table class='table table-striped'>
-    <tr>
-        <th>
-            Price
-        </th>
-        <th>
-            Merchant
-        </th>
-        <th>
-            Date
-        </th>
-        <th></th>
-    </tr>
-    
-    <form name="deleteForm">
-        {expenses_listings}
-            <tr>
-                <td>
-                    ${amount}
-                </td>
-                <td>
-                    {merchant}
-                </td>
-                <td>
-                    {datestamp}
-                </td>
+<form name="deleteForm">
+    <table class='table table-striped'>
+        <tr>
+            <th>
+                Price
+            </th>
+            <th>
+                Merchant
+            </th>
+            <th>
+                Date
+            </th>
+            <th></th>
+        </tr>
 
-                <td>
-                    <input name="deletes" id="deletes-{id}" value="{id}" type="checkbox">
-                </td>
-            </tr>
-        {/expenses_listings}
-    </form>
-    
-    <tr>
-        <td colspan="4" align="right">
-            <button class="btn btn-danger btn-sm">Delete</button>
-        </td>
-    </tr>
-    
-    <tr>
-        <td colspan="4" align="center">
-            <strong>
-                Total: ${total}
-            </strong>
-        </td>
-    </tr>
-</table>
+
+            {expenses_listings}
+                <tr>
+                    <td>
+                        ${amount}
+                    </td>
+                    <td>
+                        {merchant}
+                    </td>
+                    <td>
+                        {datestamp}
+                    </td>
+
+                    <td>
+                        <input name="deletes" id="deletes-{id}" value="{id}" type="checkbox">
+                    </td>
+                </tr>
+            {/expenses_listings}
+
+
+        <tr>
+            <td colspan="4" align="right">
+                <button class="btn btn-danger btn-sm">Delete</button>
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan="4" align="center">
+                <strong>
+                    Total: ${total}
+                </strong>
+            </td>
+        </tr>
+    </table>
+</form>
