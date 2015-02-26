902b5662220971184ed7b52b09843ffa0173b9b8
blindly <wk@mailsetup.us>
2015-02-26 17:37:39 -0500

    haaaands

--- application/controllers/Expenses.php
+++ application/controllers/Expenses.php
@@ -50,7 +50,7 @@ class Expenses extends CI_Controller {
         
         $data = array(
             'uuid'          => $this->session->gorillaUuid,
-            'amount'        => number_format( $this->input->post('amount'), 2 ),
+            'amount'        => number_format( $this->input->post('amount'), 2, '.', ',' ),
             'category'      => ucfirst( $this->input->post('category') ),
             'merchant'      => ucfirst( $this->input->post('merchant') ),
             'location'      => ucfirst( $this->input->post('location') ),
