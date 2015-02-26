86b2c57bdbff0b63b2c4125be1dfbaba45c1ca15
blindly <wk@mailsetup.us>
2015-02-26 09:34:51 -0500

    haaaands

--- application/views/user/register.php
+++ application/views/user/register.php
@@ -4,6 +4,6 @@
         <h1>Welcome</h1>
         <h2>Your UUID is <?= $gorillaUuid ?></h2>
         <p class="lead">You have just registered! Bookmark the link below to log your expenses.</p>
-        <p><a class="btn btn-lg btn-success" href="/user/u/<?= $uuid ?>" role="button">Click to Proceed</a></p>
+        <p><a class="btn btn-lg btn-success" href="/user/u/<?= $gorillaUuid ?>" role="button">Click to Proceed</a></p>
     </div>
 </div>
