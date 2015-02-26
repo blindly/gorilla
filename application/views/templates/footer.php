09a6f87208092862eeb6595286321cee381af714
blindly <wk@mailsetup.us>
2015-02-26 09:57:20 -0500

    haaaands

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,23 +1,5 @@
-    <!-- Footer -->
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <ul class="list-inline">
-                        <li><a href="/home">Home</a></li>
-                        <?php if ( $gorillaUuid ) : ?>
-                            <li><a href="/expenses">Expenses</a></li>
-                            <li><a href="/mileage">Mileage</a></li>
-                        <?php endif; ?>
-                      <li><a href="/help">Help</a></li>
-                    </ul>
-                    <p class="copyright text-muted small">Copyright &copy; Your Company 2014. All Rights Reserved</p>
-                </div>
-            </div>
-        </div>
-    </footer>
 
-    <center><em>&copy; Borke.US 2015</em></center>
+        <center><em>&copy; Borke.US 2015</em></center>
 
         <div id="debug" class="well text-center">
             <p>Debug</p>
@@ -27,6 +9,25 @@
 
             </p>
         </div>
+
+        <!-- Footer -->
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <ul class="list-inline">
+                            <li><a href="/home">Home</a></li>
+                            <?php if ( $gorillaUuid ) : ?>
+                                <li><a href="/expenses">Expenses</a></li>
+                                <li><a href="/mileage">Mileage</a></li>
+                            <?php endif; ?>
+                          <li><a href="/help">Help</a></li>
+                        </ul>
+                        <p class="copyright text-muted small">Copyright &copy; Your Company 2014. All Rights Reserved</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
 
         <!-- Bootstrap core JavaScript
         ================================================== -->
