3754f81b8ec2018dd04e185d39ca0d49937ef5f4
blindly <wk@mailsetup.us>
2015-02-28 09:20:18 -0500

    haaaands

--- application/controllers/Bills.php
+++ application/controllers/Bills.php
@@ -79,7 +79,7 @@ class Bills extends CI_Controller {
         // Get All Mileage Expenses
         $this->db->where('uuid', $this->session->gorillaUuid);
         //$this->db->where('timestamp = DATE_SUB(NOW(), INTERVAL 1 MONTH)');
-        $this->db->order_by('week', 'desc');
+        $this->db->order_by('week', 'asc');
         $bills = $this->db->get('bills');
 
         if ($bills->num_rows() > 0 )
@@ -88,17 +88,9 @@ class Bills extends CI_Controller {
             foreach ($bills->result() as $bill)
             {
                 $total = $total + $bill->amount;
-                
-                echo "<pre>";
-                print_r($total);
-                echo "</pre>";
             }
             
             $grandTotal = $grandTotal + $total;
-
-            echo "<pre>";
-            print_r($grandTotal);
-            echo "</pre>";
             
             $data = array(
                 'total' => number_format((float)$total, 2, '.', ''),
